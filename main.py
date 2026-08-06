b3cae0313ee809f29c5c46e5bec120e8d7396577
Amphibiar <138299156+Pranav-Lejith@users.noreply.github.com>
2026-08-06 22:41:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,650 @@
+####n1=int(input('Enter no1: '))
+##n2=int(input('Enter no2: '))
+##n3=int(input('Enter no3: '))
+##s=n1+n2+n3
+##print("Three numbers are: ",n1,n2,n3)
+##print('Sum is ',s)
+
+##l=float(input('Enter len: '))
+##b=float(input('Enter breadth: '))
+##a=l*b
+##print('Rectangle specs: ')
+##print('length= ',l)
+##print('breadth= ',b)
+##print('Area = ',a)
+
+##w=float(input('Enter weight: '))
+##h=float(input('Enter height: '))
+##bmi=w/(h*h)
+##print('BMI is ',bmi)
+
+##km=int(input('Enter km: '))
+##m=km*0.621371
+##print('km = ',km)
+##print('miles = ',m)
+
+##t=float(input('Enter tonnes: '))
+##q=t*10
+##k=q*100
+##print('tonnes = ',t)
+##print('quintals = ',q)
+##print('kg = ',k)
+
+##x=int(input('Enter the value of x: '))
+##y=int(input('Enter the value of y: '))
+##z=int(input('Enter the value of z: '))
+##print('Indicidually X, Y and Z are ',x,y,z)
+##d=(x*y*x)/(x*y + y*z + x*z)
+##print('Together a b and c takes the days as ',d)
+
+##x=int(input('Enter v1: '))
+##y=int(input('Enter v2: '))
+##su=x+y
+##diff=x-y
+##mul=x*y
+##div=x/y
+##mod=x%y
+##print('Numbers are ',x,y)
+##print('Sum = ',su)
+##print('Diff = ',diff)
+##print('Div = ',div)
+##print('Mod = ',mod)
+##print('Prod = ',mul)
+
+##c=float(input('Enter temp in celcius: '))
+##print('Temp in celcuis is: ',c)
+##f=c*9/5 + 32
+##print('Temp in fahrehheit= ',f)
+
+
+# -----------------------------------------PREDICT THE OUTPUT QUESTIONS----------------------------------------
+
+##x=40
+##y=x+1
+##x=20,y+x
+##print(x,y)
+
+##x,y=20,60
+##x,y=y,x
+##print(x,y)
+
+##x,y=60,30
+##x,y,x=y-10,x,x+10
+##print(x,y)
+
+##a,b,c=10,20,30
+##p,q,r=c-5,a+3,b-4
+##print(a,b,c)
+##print(p,q,r)
+
+##a,b,c=2,3,4
+##a,b,c=a*a,a*b,a*c
+##print(a,b,c)
+
+##a=5
+##b=10
+##a+= a+10
+##b*= a+b
+##print(a,b)
+
+##p=10
+##q=20
+##p*= q//3
+##q+= p+q**2
+##print(p,q)
+
+##p=5/2
+##q=p*4
+##r=p+1
+##p+= p+q+r
+##r+=                NEED TO CHECK
+##q-=p+q*r
+##print(p,q,r)
+
+##p=2/5
+##q=p*4
+##r=p+q
+##p+=p+q-r
+##r*=p-q+r
+##q+=p+q
+##print(p,q,r)
+
+#-------------------------------------------------AFTER PRINT FUNCTION-------------------------------------
+
+##n=str(input('Enter name: '))
+##c=int(input('Enter class: '))
+##s=str(input('Enter section: '))
+##print('Name: ',n)
+##print('Class: ',c)
+##print('Section: ',s)
+
+##r=float(input('Enter radius: '))
+##a=3.14*(r*r)
+##print('Area= ',a)
+
+##h=float(input('Enter height in  cm: '))
+##i=h/2.54
+##f=i/12
+##print(f'Height in cm = {h} \nHeight in inch = {i} \nHeight in foot= {f}')
+
+##n1=int(input('Enter n1: '))
+##n2=int(input('Enter n2: '))
+##n3=int(input('Enter n3: '))
+##print('The three numbers are: ',n1,n2,n3)
+##n1,n2=n1+n2,n2+n3
+##print(f'Swapped number: ',n1,n2,n3)
+
+##p=int(input('Enter principal: '))
+##n=int(input('Enter no of years: '))
+##r=int(input('Enter rate of interest: '))
+##si=(p*n*r)/100
+##print('SI = ',si)
+
+##a=5
+##b=a*2
+##c=b-1
+##print(a,b,c,sep='@')
+
+##a=int(input('Enter a number: '))
+##print(a*1,a*2,a*3,a*4,a*5,sep='\n')
+
+##n=input('Enter name: ')
+##e=int(input('Enter english marks: '))
+##p=int(input('Enter physics marks: '))
+##m=int(input('Enter maths marks: '))
+##cs=int(input('Enter cs marks: '))
+##c=int(input('Enter chem marks: '))
+##t=e+p++cs+c
+##avg=t/5
+##print('Name = ',n,'\nTotal= ',t,'\nAverage = ',avg)
+
+##n=int(input('Enter a no: '))
+##print('Square = ',n*n)
+##print('Cube = ',n*n*n)
+##print('Power 4 = ',n*n*n*n)
+
+##d=int(input('Enter no of days: '))
+##sd=d*3600*24
+##h=int(input('Enter the no of hours: '))
+##sh=h*3600
+##m=int(input('Enter no of mins: '))
+##sm=m*60
+##s=int(input('Enter no of s: '))
+##print('No of seconds = ',sd+sh+sm+s)
+
+# ------Q12 already done as obs program------
+
+##f=float(input('Enter farenheit: '))
+##print('Celcius= ',(5/9)*(f-32))
+
+##d=int(input('Enter dollars: '))
+##print('Rupees: ',d*95)
+
+##cg=int(input('Enter cost of goods: '))
+##rg=int(input('Enter the revenue generated: '))
+##oc=int(input('Enter operating cost: '))
+##print('Net profit = ',rg-cg-oc)
+##print('Gross profit = ',rg-cg)
+##print('Net profit percentage = ',(rg-cg-oc)/rg *100 )
+
+
+##m=float(input('Enter miles: '))
+##print('Kilometres = ',m/0.62)
+
+#-------------------------------------------TYPE B QUESTIONS----------------------------------------------
+
+##X=10
+##X=X+10
+##X=X-5
+##print(X)
+##X,Y = X-2,22
+##print(X,Y)
+
+##f=2
+##s=3
+##t=f*s
+##print(f,s,t)
+##f=f+s+t
+##t=s*f
+##print(f,s,t)
+
+##s=int(input('Side: '))
+##a=s*s
+##print(s,a)
+
+##x=40
+##y=x+1
+##x=20,y+x
+##print(x,y)
+
+##a,b=12,13
+##c,b=a*2,a/2
+##print(a,b,c)
+
+##x,y=20,60
+##y,x,y=x,y-10,x+10
+##print(x,y)
+
+##a,b=12,13
+##print(print(a+b))
+
+##temp=int(input('Enter unit: '))
+##if temp<=100:
+##     print(f"Free")
+##elif temp>=101 and temp<=300:
+##     print('Bill = ',2.50*(temp-100))
+##elif temp>=301 and temp<=400:
+##     print('Bill = ',4.75*(temp-100))
+##elif temp>=401 and temp<=500:
+##     print('Bill = ',6.24*(temp-100))
+##else:
+##     print('Bill = ',(temp-100)*12)
+
+
+##n=int(input('Enter a Number: '))
+##print((100*n)+(10*(n+1))+(n+2))
+
+##n1=int(input('Enter n1: '))
+##n2=int(input('Enter n2: '))
+##n3=int(input('Enter n3: '))
+##n1,n2=n1+n2,n2+n3
+##print('After swap: ',n1,n2,n3)
+##
+
+#-----------------------------------------------EB BILL----------------------------------------------------
+##units = float(input("Enter the number of units consumed: "))
+##
+##if units < 0:
+##    print("Units consumed cannot be negative.")
+##else:
+##    
+##    if units <= 100:
+##        total_bill = 0.0
+##    elif units <= 200:
+##        total_bill = (units - 100) * 2.50
+##    elif units <= 300:
+##        total_bill = (100 * 2.50) + (units - 200) * 4.75
+##    elif units <= 400:
+##        total_bill = (100 * 2.50) + (100 * 4.75) + (units - 300) * 6.25
+##    else:
+##        total_bill = ((100 * 2.50) + (100 * 4.75) + (100 * 6.25) + (units - 400) * 12.0)
+##
+##    print(f"Total Electricity Bill: {total_bill}")
+##            
+#----------------------------------------------EB BILL END--------------------------------------------------
+
+##joke = input() 
+
+##    
+##a,b,c=10,20,30
+##p,q,r= c-5,a+3,b-4
+##print('a,b,c= ',a,b,c,end='')
+##print('p,q,r= ',p,q,r,end='')
+
+##print(x=y=5)
+
+##a=input('value')
+##b=a/2
+##print(a,b)
+
+##n=input('Whats ur name: ')
+##print('Hi',n,',')
+##print('How are you doing')
+
+# the expression
+##x=int(input('Enter x: '))
+##y=int(input('Enter y: '))
+##z=int(input('Enter z: '))
+##print((4*(x**4))+(3*(y**3))+(9*z)+(6*3.14))
+
+##s=int(input('Seconds: '))
+##m=s//60
+##mods=s%60
+##print('Mins: ',m,'\nSeconds: ',mods)
+
+##y=int(input('Years; '))
+##d=365*y
+##h=d*24
+##s=h*60
+##print(f"Days: {d}\nHours: {h}\nSeconds: {s}")
+
+# wap to accspt a 3 dig no and rev the no
+
+##n=int(input('enter a 3 digit no: '))
+##r=n%10
+##r1=r
+##n=n//10
+##r=n%10
+##r2=r
+##n=n//10
+##r=n%10
+##r3=r
+##print((r1*100)+(r2*10)+r3)
+
+##import math      ##---------------------------------IMPORT--------------------------------------
+##va=int(input('Enter area: '))
+####print("Radius = ",math.sqrt(va/(4*3.14)))
+##print("Radius = ",(va/(4*3.14))**0.5)
+
+
+##r=int(input('Enter radius: '))
+##h=int(input('Enter height: '))
+##print('Volume = ',3.14*(r*r)*h)
+
+##r=int(input('Enter radius: '))
+##print('Volume = ',(4*3.14*(r*r*r))/3)
+
+##s=input('Enter string: ')
+##l=len(s)
+##i=1
+####for i in range(l):              #Test
+####     print(s)
+##while i<=l:
+##     i+=1
+##     print(s)
+     
+##y=int(input('Enter yr: '))
+##if y%400==0 and y%100==0:
+##     print('Leap')
+##elif y%4==0:
+##     print('leap')
+##else:
+##     print('Not leap')
+
+##import math
+##a=int(input('Enter s1: '))
+##b=int(input('Enter s2: '))
+##ang=int(input('Enter angle: '))
+##print('Side = ',math.sqrt((a*a)+(b*b)-(2*a*b*math.cos(ang))))
+
+##n1=int(input('Enter n1: '))
+##n2=int(input('Enter n2: '))
+##if n1%n2==0:
+##     print('Divisible')
+##else:
+##     print('Not divisible')
+
+
+##d=int(input('D: '))
+##m=int(input('M: '))
+##mc=(m-1)*30
+##print(mc+d)
+
+##p=int(input('Ente p: '))
+##r=int(input('Ente r: '))
+##t=int(input('Ente t: '))
+##print('CI= ',((p*(1+r))/100)**t)
+
+##t1=float(input('Enter temp1: '))
+##t2=float(input('Enter temp2: '))
+##t3=float(input('Enter temp3: '))
+##t4=float(input('Enter temp4: '))
+##t5=float(input('Enter temp5: '))
+##t6=float(input('Enter temp6: '))
+##t7=float(input('Enter temp7: '))
+##print('avg = ',(t1+t2+t3+t4+t5+t6+t7)/7)
+
+
+#---------------------------------------------------------------------------
+##num = int ( input( "Enter an integer :" ) )
+##if num % 2 == 0 :
+##    print (num, "is EVEN number.")
+##else :
+##    print (num, "is ODD number.")
+
+
+
+##x = y = z = 0
+##x = float (input( "Enter first number :" ) )
+##y = float (input( "Enter second number :" ) )
+##z = float (input( "Enter third number :" ) )
+##
+##max = x
+##if y > max :
+##    max = y
+##if z > max :
+##    max = z
+##print ("Largest number is", max)
+
+
+
+##sum1 = sum2 = 0
+##num1 = int(input("Enter number 1 : "))
+##num2 = int(input("Enter number 2 : "))
+##num3 = int(input("Enter number 3 : "))
+##
+##sum1 = num1 + num2 + num3
+##if num1 != num2 and num1 != num3 :
+##    sum2 += num1
+##if num2 != num1 and num2 != num3 :
+##    sum2 += num2
+##if num3 != num1 and num3 != num2 :
+##    sum2 += num3
+##
+##print("Numbers are", num1, num2, num3)
+##print("Sum of three given numbers is", sum1)
+##print("Sum of non-duplicate numbers is", sum2)
+
+
+
+##number1 = int(input("Enter first number :"))
+##number2 = int(input("Enter second number :"))
+##remainder = number1 % number2
+##if remainder == 0 :
+##    print(number1,"is divisible by", number2)
+##else :
+##    print(number1,"is not divisible by", number2)
+
+
+
+##print("Enter five numbers below")
+##num1 = float(input("First number ;"))
+##num2 = float(input("Second number ;"))
+##num3 = float(input("Third number ;"))
+##num4 = float(input("Fourth number ;"))
+##num5 = float(input("Fifth number :"))
+##divisor = float(input("Enter divisor number ;"))
+##count = 0
+##print("Multiples of", divisor, "are:")
+##remainder = num1 % divisor
+##if remainder == 0 :
+##    print(num1, sep=" ")
+##    count += 1
+##remainder = num2 % divisor
+##if remainder == 0 :
+##    print(num2, sep=" ")
+##    count += 1
+##remainder = num3 % divisor
+##if remainder == 0 :
+##    print(num3, sep=" ")
+##    count += 1
+##remainder = num4 % divisor
+##if remainder == 0 :
+##    print(num4, sep=" ")
+##    count += 1
+##remainder = num5 % divisor
+##if remainder == 0 :
+##    print(num5, sep=" ")
+##    count += 1
+##print()
+##print(count, "multiples of", divisor, "found")
+
+
+
+##radius = float ( input ( "Enter radius of the circle :" ))
+##print ("1. Calculate Area")
+##print ("2. Calculate Perimeter")
+##choice = int (input( "Enter your choice (1 or 2) :" ))
+##if choice == 1 :
+##    area = 3.14159 * radius * radius
+##    print ("Area of circle with radius", radius, 'is', area)
+##else :
+##    perm = 2 * 3.14159 * radius
+##    print ("Perimeter of circle with radius", radius, 'is', perm)
+
+
+
+##num1 = float ( input( "Enter first number :" ) )
+##num2 = float ( input( "Enter second number :" ) )
+##op = input( "Enter operator [ + - * / % ] :" )
+##result = 0
+##if op == '+' :
+##    result = num1 + num2
+##elif op == '-' :
+##    result = num1 - num2
+##elif op == '*' :
+##    result = num1 * num2
+##elif op == '/' :
+##    result = num1 / num2
+##elif op == '%' :
+##    result = num1 % num2
+##else :
+##    print ("Invalid operator!!")
+##print (num1, op, num2, '=', result)
+
+
+
+##x = int(input( "Enter first number :" ))
+##y = int(input( "Enter second number :" ))
+##z = int(input( "Enter third number :" ))
+##min = mid = max = None
+##
+##if x < y and x < z :
+##    if y < z :
+##        min, mid, max = x, y, z
+##    else :
+##        min, mid, max = x, z, y
+##elif y < x and y < z :
+##    if x < z :
+##        min, mid, max = y, x, z
+##    else :
+##        min, mid, max = y, z, x
+##else :
+##    if x < y :
+##        min, mid, max = z, x, y
+##    else :
+##        min, mid, max = z, y, x
+##
+##print ("Numbers in ascending order :", min, mid, max)
+
+
+
+##ch = input( "Enter a single character :" )
+##if ch >= 'A' and ch <= 'Z' :
+##    print ("You entered an Upper case character.")
+##
+##elif ch >= 'a' and ch <= 'z' :
+##    print ("You entered a lower case character.")
+##
+##elif ch >= '0' and ch <= '9' :
+##    print ("You entered a digit.")
+##
+##else :
+##    print ("You entered a special character.")
+
+
+
+##import math
+##
+##print ("For quadratic equation, ax**2 + bx + c = 0, enter coefficients below")
+##a = int( input ( "Enter a : " ) )
+##b = int( input ( "Enter b : " ) )
+##c = int( input ( "Enter c : " ) )
+##
+##if a == 0 :
+##    print ("Value of", a, 'should not be zero')
+##    print ("\n Aborting !!!!!!" )
+##else :
+##    delta = b * b - 4 * a * c
+##    if delta > 0 :
+##        root1 = ( -b + math.sqrt(delta) ) / (2 * a)
+##        root2 = ( -b - math.sqrt(delta) ) / (2 * a)
+##        print ("Roots are REAL and UNEQUAL")
+##        print ("Root1 =", root1, ", Root2 =", root2)
+##    elif delta == 0 :
+##        root1 = -b / (2 * a)
+##        print ("Roots are REAL and EQUAL")
+##        print ("Root1 =", root1, "\nRoot2 =", root1)
+##    else :
+##        print ("Roots are COMPLEX and IMAGINARY")
+
+
+##num = int(input("Enter a 6 digit number : "))
+##if num < 100000 or num > 999999 :
+##    print("Please enter a 6 digit number")
+##else:
+##    n1 = num % 100
+##    int1 = num // 100
+##    n2 = int1 % 100
+##    int2 = int1 // 100
+##    n3 = int2 % 100
+##    print("Three 2-digit numbers are:", n1, n2, n3)
+
+##a1 = a2 = a3 = 0
+##a1 = float(input("Enter angle 1 : "))
+##a2 = float(input("Enter angle 2 : "))
+##a3 = float(input("Enter angle 3 : "))
+##if (a1 + a2 + a3) == 180 :
+##    print("Angles form a triangle.")
+##else:
+##    print("Angles do not form a triangle.")
+
+
+##w = float(input("Enter weight in kg :"))
+##h = float(input("Enter height in meters :"))
+##bmi = w/ ( h * h)
+##print("BMI is : ", bmi, end = " ")
+##if bmi < 18.5 :
+##    print("Underweight")
+##elif bmi < 25 :
+##    print("Normal")
+##elif bmi < 30 :
+##    print("Overweight")
+##else:
+##    print("Obese")
+
+
+
+##yr = int(input("Enter year :"))
+##if yr % 100 == 0 : 
+##    if yr % 400 == 0 :
+##        leap = True
+##    else :
+##        leap = False
+##elif yr % 4 == 0 :
+##    leap = True
+##else :
+##    leap = False
+##if leap == True :
+##    print(yr, "is a leap year")
+##else :
+##    print(yr, "is not a leap year")
+
+
+
+##m = float(input("Enter marks : "))
+##if m >= 90 :
+##    grade = 'A'
+##elif m >= 80 :
+##    grade = 'B'
+##elif m >= 70 :
+##    grade = 'C'
+##elif m >= 60 :
+##    grade = 'D'
+##else :
+##    grade = 'E'
+##print("For marks", m, "the Grade is", grade)
+
+
+
+
+
+
+
+
+##x = float (input ( "Enter value of x :" ))
+##n = int (input( "Enter value of n (for x ** n) :" ))
+##s = 0
+##for a in range(n + 1) :
+##    s += x ** a
+##print ("Sum of first" , n , "terms :", s)
---
+++ output_main.py
@@ -0,0 +1,122 @@
+Python 3.12.7 (tags/v3.12.7:0b05ead, Oct  1 2024, 03:06:41) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: C:\Users\lejit\Downloads\main.py ==================
+
+=================== RESTART: C:\Users\lejit\Downloads\main.py ==================
+Enter an integer :2
+2 is EVEN number.
+
+=================== RESTART: C:\Users\lejit\Downloads\main.py ==================
+Enter first number :5
+Enter second number :8
+Enter third number :2
+Largest number is 8.0
+
+=================== RESTART: C:\Users\lejit\Downloads\main.py ==================
+Enter number 1 : 2
+Enter number 2 : 6
+Enter number 3 : 3
+Numbers are 2 6 3
+Sum of three given numbers is 11
+Sum of non-duplicate numbers is 11
+
+=================== RESTART: C:\Users\lejit\Downloads\main.py ==================
+Enter first number :3
+Enter second number :76
+3 is not divisible by 76
+
+=================== RESTART: C:\Users\lejit\Downloads\main.py ==================
+Enter five numbers below
+First number ;23
+Second number ;24
+Third number ;25
+Fourth number ;26
+Fifth number :27
+Enter divisor number ;5
+Multiples of 5.0 are:
+25.0
+
+1 multiples of 5.0 found
+
+=================== RESTART: C:\Users\lejit\Downloads\main.py ==================
+Enter radius of the circle :2
+1. Calculate Area
+2. Calculate Perimeter
+Enter your choice (1 or 2) :1
+Area of circle with radius 2.0 is 12.56636
+
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+Enter first number :2
+Enter second number :3
+Enter operator [ + - * / % ] :*
+2.0 * 3.0 = 6.0
+
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+Enter first number :5
+Enter second number :2
+Enter third number :87
+Numbers in ascending order : 2 5 87
+
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+Enter a single character :S
+You entered an Upper case character.
+
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+For quadratic equation, ax**2 + bx + c = 0, enter coefficients below
+Enter a : 2
+Enter b : 6
+Enter c : 3
+Roots are REAL and UNEQUAL
+Root1 = -0.6339745962155614 , Root2 = -2.3660254037844384
+
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+For quadratic equation, ax**2 + bx + c = 0, enter coefficients below
+Enter a : 2
+Enter b : 4
+Enter c : 2
+Roots are REAL and EQUAL
+Root1 = -1.0 , Root2 = -1.0
+
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+For quadratic equation, ax**2 + bx + c = 0, enter coefficients below
+Enter a : 2
+Enter b : 4
+Enter c : 2
+Roots are REAL and EQUAL
+Root1 = -1.0 
+Root2 = -1.0
+>>> 
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+Enter a 6 digit number : 231120
+Three 2-digit numbers are: 20 11 23
+Enter angle 1 : 
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+Enter a 6 digit number : 231120
+Three 2-digit numbers are: 20 11 23
+>>> 
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+Enter angle 1 : 60
+Enter angle 2 : 60
+Enter angle 3 : 60
+Angles form a triangle.
+>>> 
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+Enter weight in kg :50
+Enter height in meters :1.64
+BMI is :  18.590124925639504 Normal
+>>> 
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+Enter year :2016
+2016 is a leap year
+>>> 
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+Enter marks : 90\
+Traceback (most recent call last):
+  File "C:\Users\lejit\Downloads\main.py", line 625, in <module>
+    m = float(input("Enter marks : "))
+ValueError: could not convert string to float: '90\\'
+>>> 
+================================================= RESTART: C:\Users\lejit\Downloads\main.py =================================================
+Enter marks : 90
+For marks 90.0 the Grade is A
